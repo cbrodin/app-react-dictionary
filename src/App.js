5bd9a780b109635272f820fc972068727b333a18
Chelsea Brodin <brodinchelsea@gmail.com>
2022-10-27 21:19:11 -0500

    Started build for Dictionary app with react, imported react and bootstrap, added main header logo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="Dictionary.png"
+          alt="Dictionary logo"
+          width="200"
+          height="300"
+          className="logo"
+        ></img>
+        Hello World!
+      </header>
+    </div>
+  );
+}
+
+export default App;
